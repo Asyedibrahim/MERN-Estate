ba915c058e5699c6b8ee12c6f403f965c47bd5e0
Asyedibrahim <syedibrahim7252@gmail.com>
2024-05-11 15:10:01 +0530

    Header Created

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+
+    <Header />
+
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
@@ -15,6 +19,7 @@ export default function App() {
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
     </Routes>
+
     </BrowserRouter>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import {FaSearch} from 'react-icons/fa';
+import { FaHome } from "react-icons/fa";
+import { MdMessage } from "react-icons/md";
+import { FaSignInAlt } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+
+          <Link to="/" >
+            <h1 className='font-bold text-md sm:text-xl flex flex-wrap'>
+                <span className='text-green-500'>Zoro</span>
+                <span className='text-slate-700'>Estate</span>
+            </h1>
+          </Link>
+          
+            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                <input 
+                  type="text" 
+                  placeholder='Search...' className='bg-transparent focus:outline-none w-28 sm:w-64'
+                />
+                <FaSearch className="text-slate-600" />
+            </form>
+
+            <ul className='flex gap-7'>
+              <Link to="/" className='flex items-center gap-1'>
+                <FaHome />
+                <li className='hidden sm:inline text-slate-700 hover:underline font-bold'>Home</li>
+              </Link>
+              <Link to="about" className='flex items-center gap-1'>
+                <MdMessage />
+                <li className='hidden sm:inline text-slate-700 hover:underline font-bold'>About</li>
+              </Link>
+              <Link to="/sign-in" className='flex items-center gap-1'>
+                <FaSignInAlt/>
+                <li className='text-slate-700 hover:underline font-bold'>Sign In</li>
+              </Link>
+            </ul>
+
+        </div>
+    </header>
+  )
+}
